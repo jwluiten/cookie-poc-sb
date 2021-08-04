4e51b6783a654b32d81de52cd93b3f2b6b2beefd
Jan Willem Luiten <janwillem.luiten@gmail.com>
2021-08-04 22:22:24 +0200

    Initial commit

---
+++ src/main/java/com/luiten/cookiepoc/CookiePocApplication.java
@@ -0,0 +1,13 @@
+package com.luiten.cookiepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookiePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookiePocApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/luiten/cookiepoc/controllers/RedirectController.java
@@ -0,0 +1,55 @@
+package com.luiten.cookiepoc.controllers;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Slf4j
+@RestController
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Cookie POC controller",
+        version = "1.0"
+    )
+)
+public class RedirectController {
+
+    @GetMapping(path = "/redirect")
+    @Operation(
+        summary = "Redirection endpoint",
+        responses = {
+            @ApiResponse(responseCode = "302", description = "redirect to requested URL")
+        }
+    )
+    public Mono<ResponseEntity<Void>> getRedirect(
+        @RequestParam("url") String url
+    ) {
+        return Mono.just(ResponseEntity
+            .status(HttpStatus.FOUND)
+            .headers(headers -> headers.add(HttpHeaders.LOCATION, url))
+            .headers(headers -> headers.add(HttpHeaders.SET_COOKIE, buildCookie("cxrsProcessed", "true",60000).toString()))
+            .headers(headers -> headers.add(HttpHeaders.SET_COOKIE, buildCookie("survivor", "kilroy",24* 60* 60 * 1000).toString()))
+            .build());
+    }
+
+    private static ResponseCookie buildCookie(String name, String value, int durationInMillis) {
+        return ResponseCookie
+            .from(name, value)
+            .path("/")
+            .maxAge(Duration.ofMillis(durationInMillis))
+            .httpOnly(true)
+            .build();
+    }
+}
