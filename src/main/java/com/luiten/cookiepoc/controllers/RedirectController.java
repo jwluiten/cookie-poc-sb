bc6f53f6d1d9d4e66ae02700e4c50871e86896e8
Jan Willem Luiten <janwillem.luiten@gmail.com>
2021-08-05 11:31:54 +0200

    add page that sets cookie

--- src/main/java/com/luiten/cookiepoc/controllers/RedirectController.java
+++ src/main/java/com/luiten/cookiepoc/controllers/RedirectController.java
@@ -4,19 +4,19 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
+import static com.luiten.cookiepoc.utilities.CookieBuilder.buildCookie;
 
-@Slf4j
+@Log4j2
 @RestController
 @OpenAPIDefinition(
     info = @Info(
@@ -38,18 +38,12 @@ public class RedirectController {
     ) {
         return Mono.just(ResponseEntity
             .status(HttpStatus.FOUND)
-            .headers(headers -> headers.add(HttpHeaders.LOCATION, url))
-            .headers(headers -> headers.add(HttpHeaders.SET_COOKIE, buildCookie("cxrsProcessed", "true",60000).toString()))
-            .headers(headers -> headers.add(HttpHeaders.SET_COOKIE, buildCookie("survivor", "kilroy",24* 60* 60 * 1000).toString()))
+            .headers(headers -> headers
+                .add(HttpHeaders.LOCATION, url))
+            .headers(headers -> headers
+                .add(HttpHeaders.SET_COOKIE, buildCookie("cxrsProcessed", "true",60000).toString()))
+            .headers(headers -> headers
+                .add(HttpHeaders.SET_COOKIE, buildCookie("survivor", "kilroy",24* 60* 60 * 1000).toString()))
             .build());
-    }
-
-    private static ResponseCookie buildCookie(String name, String value, int durationInMillis) {
-        return ResponseCookie
-            .from(name, value)
-            .path("/")
-            .maxAge(Duration.ofMillis(durationInMillis))
-            .httpOnly(true)
-            .build();
     }
 }
